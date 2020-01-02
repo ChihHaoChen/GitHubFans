c7e28d7d567038791c5cfa55c7b67a39c3ff8a20
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2020-01-02 11:51:39 +0800

    Initial Commit

---
+++ GitHub_Fans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub_Fans
+//
+//  Created by ChihHao on 2020/01/02.
+//  Copyright © 2020 ChihHao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
