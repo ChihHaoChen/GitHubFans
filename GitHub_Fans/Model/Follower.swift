6297eaa26ce58765430fd5aaf29b4754943a2508
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2020-01-03 18:54:21 +0800

    Add model files for the followers and users.

---
+++ GitHub_Fans/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitHub_Fans
+//
+//  Created by ChihHao on 2020/01/03.
+//  Copyright © 2020 ChihHao. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable	{
+	var login: String,
+	var avatarUrl: String
+}
