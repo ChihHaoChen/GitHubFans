de56fe3acba2fff738c8508f70aa85149f583578
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2020-02-03 23:52:48 +0800

    Refactor the code of GFAlertVC.swift, and add convenience to init() @ several UI elemets to clean the codes.

---
+++ GitHub_Fans/Controller/UIElements/GFAlertContainerView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAlertswift
+//  GitHub_Fans
+//
+//  Created by ChihHao on 2020/02/03.
+//  Copyright © 2020 ChihHao. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		configure()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func configure()   {
+        
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+
+}
